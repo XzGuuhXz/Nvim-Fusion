68ddcbceefa0835c156b99e62f5cf9a7533639c6
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-05 07:09:14 +0000

    fix(trouble): correct lazy option typo

--- lua/plugins/core/trouble.lua
+++ lua/plugins/core/trouble.lua
@@ -1,6 +1,5 @@
 return {
   "folke/trouble.nvim",
-  layz = false,
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     icons = {
@@ -8,16 +7,16 @@ return {
       warning = "󰀪",
       hint = "󰌶",
       information = "󰋽",
-      other = "󰠱"
+      other = "󰠱",
     },
   },
   cmd = "Trouble",
   keys = {
-    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "󰒡 Diagnostics (Trouble)" },
-    { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "󰒡 Buffer Diagnostics (Trouble)" },
-    { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "󰘦 Symbols (Trouble)" },
-    { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "󰌘 LSP Definitions / references / ... (Trouble)" },
-    { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "󰂖 Location List (Trouble)" },
-    { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "󰂖 Quickfix List (Trouble)" },
+    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+    { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+    { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+    { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP definitions / references" },
+    { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+    { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
   },
 }

a7b65ab9643bad052e999a8289ddef6e94d3b1b0
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-05 07:11:50 +0000

    fix: enforce supported Neovim version

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,8 @@
+if vim.fn.has("nvim-0.12") ~= 1 then
+  vim.notify("Nvim-Fusion requires Neovim 0.12+.", vim.log.levels.ERROR)
+  return
+end
+
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")

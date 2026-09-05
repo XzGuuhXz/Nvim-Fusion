bbe0c5ad15c99a71e9d81e4efdeb0ba2ed3b0179
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-05 07:09:21 +0000

    fix(keymaps): remove leader conflicts and duplicate mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,23 +1,22 @@
--- Keymaps básicos
+-- Core keymaps
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" })
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
-vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+vim.keymap.set("n", "<leader>fs", "<cmd>write<cr>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>qq", "<cmd>quit<cr>", { desc = "Quit window" })
 
--- Navegação entre janelas
+-- Window navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 
--- Mover linhas no modo visual
+-- Move lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 
--- Melhor experiência de indentação
+-- Keep visual selection after indentation
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent right" })
 
--- Centralizar cursor em buscas
+-- Center cursor after searches
 vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result" })
-

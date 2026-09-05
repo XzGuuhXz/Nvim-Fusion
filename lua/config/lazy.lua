1636c59b8f3f98e57c0c1f28942fa34d7801677b
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-05 07:53:23 +0000

    fix: require Neovim 0.12 uv API

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,6 +1,6 @@
 -- Bootstrap lazy.nvim deterministically.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local lazy_commit = "85c7ff3711b730b4030d03144f6db6375044ae82"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", lazyrepo, lazypath })

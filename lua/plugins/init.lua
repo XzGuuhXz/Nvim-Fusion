959d0b784ae6079176db691da5d3169d9e1ca30d
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-09 15:12:53 -0300

    refactor: organize plugin imports

---
+++ lua/plugins/init.lua
@@ -0,0 +1,8 @@
+return {
+  { import = "plugins.lsp" },
+  { import = "plugins.treesitter" },
+  { import = "plugins.editor" },
+  { import = "plugins.ui" },
+  { import = "plugins.git" },
+  { import = "plugins.util" },
+}

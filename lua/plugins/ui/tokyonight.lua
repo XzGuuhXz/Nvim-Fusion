818ccd9f66d0a097b84b9c1e624134b0aaaf86b0
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-09 15:40:30 -0300

    feat(theme): activate NVIM FUSION purple colorscheme

--- lua/plugins/ui/tokyonight.lua
+++ lua/plugins/ui/tokyonight.lua
@@ -1,38 +1,11 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    dir = vim.fn.stdpath("config") .. "/lua/colors",
+    name = "nvim-fusion-theme",
     lazy = false,
     priority = 1000,
-    opts = {
-      style = "night",
-      light_style = "day",
-      transparent = false,
-      terminal_colors = true,
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = { bold = true },
-        variables = {},
-        sidebars = "dark",
-        floats = "dark",
-      },
-      sidebars = { "qf", "help", "vista_kind", "terminal", "packer" },
-      day_brightness = 0.3,
-      hide_inactive_statusline = false,
-      dim_inactive = false,
-      lualine_bold = false,
-      on_colors = function(colors)
-        colors.hint = colors.orange
-        colors.error = "#ff0000"
-      end,
-      on_highlights = function(highlights, colors)
-        highlights.LineNr = { fg = colors.orange }
-        highlights.CursorLineNr = { fg = colors.yellow, bold = true }
-      end,
-    },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd.colorscheme("tokyonight")
+    config = function()
+      vim.cmd.colorscheme("nvim-fusion")
     end,
   },
 }

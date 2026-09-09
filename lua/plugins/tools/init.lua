6f55e37c89551ae3d185ebda08cf449b8275b467
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-09 14:55:08 -0300

    fix: migrate Treesitter to current API

--- lua/plugins/tools/init.lua
+++ lua/plugins/tools/init.lua
@@ -1,48 +1,65 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
     config = function()
       require("telescope").setup({
         defaults = {
           mappings = {
             i = {
-              ["<C-h>"] = "which_key"
-            }
-          }
-        }
+              ["<C-h>"] = "which_key",
+            },
+          },
+        },
       })
-      
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-      vim.keymap.set('n', '<leader>ps', function()
+
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Git files" })
+      vim.keymap.set("n", "<leader>ps", function()
         builtin.grep_string({ search = vim.fn.input("Grep > ") })
-      end)
-      vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-    end
+      end, { desc = "Search word" })
+      vim.keymap.set("n", "<leader>pb", builtin.buffers, { desc = "Buffers" })
+    end,
   },
+
+  -- Treesitter atual: requer Neovim >= 0.12 e usa a API nativa de parser/highlight
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "lua", "vim", "vimdoc", "query",
-          "python", "javascript", "typescript",
-          "html", "css", "json", "yaml",
-          "bash", "markdown", "c", "cpp"
-        },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true
-        }
+      local parsers = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "python",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "json",
+        "yaml",
+        "bash",
+        "markdown",
+        "c",
+        "cpp",
+      }
+
+      require("nvim-treesitter").install(parsers)
+
+      local group = vim.api.nvim_create_augroup("NvimFusionTreesitter", { clear = true })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = group,
+        pattern = parsers,
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+        end,
       })
-    end
-  }
+    end,
+  },
 }

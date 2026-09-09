74a1628a3e7991329ccb4d12e38b611a6b9ad790
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-09 16:44:00 -0300

    fix(lsp): enforce Mason dependency ordering

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -3,6 +3,7 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
+      "mason-org/mason.nvim",
       "mason-org/mason-lspconfig.nvim",
     },
     config = function()

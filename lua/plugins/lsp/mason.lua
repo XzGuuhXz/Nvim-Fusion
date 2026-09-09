fe370a925d0f3db869cdcb35ce12cb7cd4bbd03f
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-09 15:13:07 -0300

    refactor: isolate mason configuration

---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    dependencies = {
+      "mason-org/mason-lspconfig.nvim",
+    },
+    opts = {
+      ui = {
+        border = "rounded",
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+  },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "pyright",
+        "ts_ls",
+        "jsonls",
+        "html",
+        "cssls",
+        "clangd",
+      },
+      automatic_installation = true,
+      automatic_enable = false,
+    },
+  },
+}

cfa903b610711fccf158f9685c5ebd1000c8c6e0
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-09 16:35:31 -0300

    fix: align telescope keymaps with documentation

--- lua/plugins/editor/telescope.lua
+++ lua/plugins/editor/telescope.lua
@@ -16,12 +16,20 @@ return {
       })
 
       local builtin = require("telescope.builtin")
+
       vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+
       vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Git files" })
+
       vim.keymap.set("n", "<leader>ps", function()
         builtin.grep_string({ search = vim.fn.input("Grep > ") })
       end, { desc = "Search word" })
+
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+
       vim.keymap.set("n", "<leader>pb", builtin.buffers, { desc = "Buffers" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
     end,
   },
 }

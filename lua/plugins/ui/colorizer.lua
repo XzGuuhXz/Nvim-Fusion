f95d6b541bacf8014b5527d7d2c0dcfe431ce9a7
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-09 15:14:40 -0300

    refactor: isolate colorizer configuration

---
+++ lua/plugins/ui/colorizer.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      filetypes = { "*" },
+      user_default_options = {
+        RGB = true,
+        RRGGBB = true,
+        names = true,
+        mode = "background",
+        AARRGGBB = true,
+        virtualtext = "■",
+      },
+      buftypes = {},
+    },
+  },
+}

be6b5e8b89171f820d192cde9e6e7b1ae9551b8f
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-09 15:14:06 -0300

    refactor: isolate which-key configuration

---
+++ lua/plugins/editor/which-key.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      preset = "modern",
+      icons = {
+        breadcrumb = "󰅂",
+        separator = "󰘔",
+        group = "󰉋",
+      },
+      win = {
+        border = "rounded",
+      },
+      layout = {
+        spacing = 3,
+      },
+    },
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      wk.add({
+        { "<leader>f", group = "󰈞 Find" },
+        { "<leader>l", group = "󰌘 LSP" },
+        { "<leader>g", group = "󰊢 Git" },
+        { "<leader>w", group = "󰖲 Window" },
+        { "<leader>b", group = "󰈚 Buffer" },
+        { "<leader>d", group = "󰃤 Diagnostics" },
+        { "<leader>c", group = "󰌌 Code" },
+        { "<leader>pf", desc = "Find files" },
+        { "<leader>ps", desc = "Search text" },
+        { "<leader>pb", desc = "Buffers" },
+        { "<leader>ff", desc = "Find files" },
+        { "<leader>fg", desc = "Live grep" },
+        { "<leader>fb", desc = "List buffers" },
+        { "<leader>fe", desc = "Find file in explorer" },
+        { "<leader>lf", desc = "Format code" },
+        { "<leader>ls", desc = "Signature help" },
+        { "<leader>rn", desc = "Rename symbol" },
+        { "<leader>ca", desc = "Code actions" },
+      })
+    end,
+  },
+}

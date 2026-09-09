17fea2a119d87e406e695fdc0f50ad0979f736b2
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-09 15:12:44 -0300

    refactor: extract diagnostics config

---
+++ lua/config/diagnostics.lua
@@ -0,0 +1,27 @@
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●",
+    spacing = 4,
+    severity_sort = true,
+    source = "if_many",
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "",
+      [vim.diagnostic.severity.INFO] = "",
+    },
+  },
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+    focusable = false,
+    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+  },
+})

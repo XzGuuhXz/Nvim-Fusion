43a052d93e32bdb10eb986d7e04b2ebe1f17f029
Gustavo Henrique Pereira Ferreira <162772087+XzGuuhXz@users.noreply.github.com>
2026-09-09 15:12:34 -0300

    refactor: extract editor options

---
+++ lua/config/options.lua
@@ -0,0 +1,27 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local options = {
+  number = true,
+  relativenumber = false,
+  tabstop = 2,
+  shiftwidth = 2,
+  expandtab = true,
+  smartindent = true,
+  wrap = false,
+  swapfile = false,
+  backup = false,
+  hlsearch = false,
+  incsearch = true,
+  termguicolors = true,
+  showmode = false,
+  scrolloff = 8,
+  signcolumn = "yes",
+  updatetime = 50,
+  colorcolumn = "0",
+  clipboard = "unnamedplus",
+}
+
+for name, value in pairs(options) do
+  vim.opt[name] = value
+end
